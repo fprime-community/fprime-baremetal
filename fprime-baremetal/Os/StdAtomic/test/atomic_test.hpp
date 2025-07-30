2c4010a2f42db021785fe78d8c599634dba08450
Mishaal Aleem <mishaalaleem@gmail.com>
2025-07-30 14:11:32 -0700

    Add 'stub' atomic implementation

---
+++ fprime-baremetal/Os/StdAtomic/test/atomic_test.hpp
@@ -0,0 +1,8 @@
+// ======================================================================
+// \title atomic_test.hpp
+// \brief wrapper for std::atomic into Test namespace
+// ======================================================================
+
+namespace TestStdAtomic {
+    #include "../atomic"
+}
