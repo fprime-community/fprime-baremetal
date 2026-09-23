bd7e50ae833fcfb9847df64f86ce1f02301ff17f
Michael Kiper <77862413+michaelkiper@users.noreply.github.com>
2026-09-23 09:19:45 -0700

    PassiveCmdDispatcher send command even when sequence table full (#40)
    * PassiveCmdDispatcher send command even when sequence table full
    
    * remove comments
    
    * cleaned up def of PassiveCmdDispatcher::seqCmd_helper
    
    * added small comment and correction
    
    * resolved C++11 issues
    
    ---------
    
    Co-authored-by: makiper <michael.kiper@jpl.nasa.gov>

--- fprime-baremetal/Svc/PassiveCmdDispatcher/PassiveCmdDispatcher.cpp
+++ fprime-baremetal/Svc/PassiveCmdDispatcher/PassiveCmdDispatcher.cpp
@@ -113,7 +113,7 @@ void PassiveCmdDispatcher::compCmdStat_handler(FwIndexType portNum,
     U32 context = 0;
     for (auto pending = 0; pending < CMD_DISPATCHER_SEQUENCER_TABLE_SIZE; pending++) {
         auto entry = &this->m_cmdTables->m_sequenceTracker[pending];
-        if ((entry->opcode != OPCODE_UNUSED) && (entry->seq == cmdSeq)) {
+        if ((entry->seq == cmdSeq) && (entry->opcode != OPCODE_UNUSED)) {
             portToCall = entry->callerPort;
             context = entry->context;
             FW_ASSERT(opCode == entry->opcode);
@@ -145,13 +145,14 @@ void PassiveCmdDispatcher::seqCmd_helper(FwIndexType portNum,
         for (auto slot = 0; slot < CMD_DISPATCHER_DISPATCH_TABLE_SIZE; slot++) {
             if (this->m_cmdTables->m_entryTable[slot].opcode == opcode) {
                 entry = &this->m_cmdTables->m_entryTable[slot];
+                break;
             }
         }
     }
-    if ((entry != nullptr) && this->isConnected_compCmdSend_OutputPort(entry->port)) {
+    if (entry != nullptr && this->isConnected_compCmdSend_OutputPort(entry->port)) {
         // Register the command in the command tracker only if the response port is connected
+        bool pendingFound = false;
         if (this->isConnected_seqCmdStatus_OutputPort(portNum)) {
-            bool pendingFound = false;
             for (U32 pending = 0; pending < CMD_DISPATCHER_SEQUENCER_TABLE_SIZE; pending++) {
                 SequenceTracker* trackerEntry = &this->m_cmdTables->m_sequenceTracker[pending];
                 if (trackerEntry->opcode == OPCODE_UNUSED) {
@@ -163,27 +164,24 @@ void PassiveCmdDispatcher::seqCmd_helper(FwIndexType portNum,
                     break;
                 }
             }
-            // If no slot was found to track the command, quit
-            if (!pendingFound) {
-                this->log_WARNING_HI_TooManyCommands(opcode);
-                if (this->isConnected_seqCmdStatus_OutputPort(portNum)) {
-                    this->seqCmdStatus_out(portNum, opcode, context, Fw::CmdResponse::EXECUTION_ERROR);
-                }
-                return;
-            }
         }
-
-        // Pass arguments to the argument buffer and log the dispatched command
+        // Pass arguments to the argument buffer and log the dispatched command.
+        // Even if we did got find a sequencer slot, still send the command as we must ensure all critical commands can
+        // be completed.
         this->compCmdSend_out(entry->port, opcode, this->m_seq, args);
         this->log_COMMAND_OpCodeDispatched(opcode, entry->port);
+
+        if (!pendingFound) {  // PassiveCmdDispatcher::compCmdStat_handler will not clear the sequence
+            this->log_WARNING_HI_TooManyCommands(opcode);
+            this->seqCmdStatus_out(portNum, opcode, context, Fw::CmdResponse::EXECUTION_ERROR);
+        }
     } else {
-        // Opcode could not be found in the dispatch table, fail the command
+        // Branch taken when opcode fails to properly resolve to a mapped connected port.
         this->log_WARNING_HI_InvalidCommand(opcode);
         if (this->isConnected_seqCmdStatus_OutputPort(portNum)) {
             this->seqCmdStatus_out(portNum, opcode, context, Fw::CmdResponse::INVALID_OPCODE);
         }
     }
-
     // Increment sequence number
     this->m_seq++;
 }
